869fb611390b2b88a09766d389f5ba1590271cd9
tisofteletronica <ti@softeletronica.com.br>
2024-04-12 20:09:27 -0300

    refactor9

--- src/main/java/com/softeletronica/intrasoft/config/AppConfig.java
+++ src/main/java/com/softeletronica/intrasoft/config/AppConfig.java
@@ -1,17 +1,15 @@
 package com.softeletronica.intrasoft.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 
 public class AppConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
