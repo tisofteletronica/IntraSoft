1fe90d4b8b72c03e5755661c8868a2ca86c9782d
tisofteletronica <ti@softeletronica.com.br>
2024-04-14 09:52:40 -0300

    corrigido bean encrypt bcrypt

--- src/main/java/com/softeletronica/intrasoft/config/AppConfig.java
+++ src/main/java/com/softeletronica/intrasoft/config/AppConfig.java
@@ -3,14 +3,13 @@ package com.softeletronica.intrasoft.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 
 public class AppConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
